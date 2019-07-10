d7b0a03aa248d0c28c04f13fc2906b55e4a5029a
lius <1986267850@qq.com>
2019-07-11 00:42:08 +0800

    nodebase

--- com.sunland.base/base1.js
+++ com.sunland.base/base1.js
@@ -1,10 +1,21 @@
+
+//  mode fs
 f=require('fs');
-function a1(){
+//sync
+function sync(){
+    data=f.readFileSync('input.txt');
+    console.log(data.toString());
+}
+
+
+//nosync
+function nosync(){
     f.readFile('input.txt',function(err,data){
         //console.log(data.toString())
         if (err) return console.error(err);
         console.log(data.toString())
     });
 }
-a1();
+
+sync();
 
